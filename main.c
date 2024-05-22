fdb9b3b7977704a97aa72fb1c1e1ac2d6f249cc7
K-Andre Harris <123778827+kdreh@users.noreply.github.com>
2024-05-22 10:38:00 -0400

    First phase
    create identify_identical functions to compare values in an array

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+void identify_identical(int value[], int n) {
+    for(int i=0; i<n; i++) {
+        for(int j=i+1; j<n; j++) {
+            if(value[i] == value[j]) {
+                printf("Twin integers found.\n");
+                return;
+            }
+        }
+    }
+    printf("No two integers are alike.\n");
+}
+
 int main(void) {
-    printf("Hello, World!\n");
+   int a[5] = {1,2,0,5,4};
+    identify_identical(a,5);
     return 0;
 }
